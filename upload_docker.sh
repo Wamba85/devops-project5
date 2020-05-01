85ec0229d18a26e56810c0fbdc4fbe12afb595e5
Andrea Mori <wamba85@gmail.com>
2020-05-01 18:31:05 +0000

    update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="wamba/python-app"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username wamba
+docker build -t python-app .
+docker tag python-app:latest wamba/python-app:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push wamba/python-app:latest
