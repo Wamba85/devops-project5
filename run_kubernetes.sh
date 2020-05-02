03ac1d2713e3dea9b0736edd3db197813e76ef21
Andrea Mori <wamba85@gmail.com>
2020-05-02 07:29:56 +0000

    update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="wamba/python-app"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run python-app\
+    --image=$dockerpath\
+    --port=80 --labels app=python-app
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward python-app 8000:80
 
